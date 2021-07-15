556a83fdadd54fe3231540022d584a07995ffb7b
taguhoiya <ouch.taguhoiya@outlook.jp>
2021-07-15 18:07:32 +0900

    react router approach

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        include ActionController::MimeResponds
     before_action :fake_load
 
     def fake_load
       sleep(1)
+    end
+
+    def fallback_index_html
+        respond_to do |format|
+            format.html { render body: Rails.root.join('public/index.html').read }
+        end
     end
 end
