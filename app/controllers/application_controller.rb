c0a34466126f18208009da188590e7b6ee4b4be0
taguhoiya <ouch.taguhoiya@outlook.jp>
2021-07-19 23:11:20 +0900

    ready for first push to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
 
     def fallback_index_html
         respond_to do |format|
-            format.html { render body: Rails.root.join('public/index.html').read }
+            format.html { render body: Rails.root.join('public/index.js').read }
         end
     end
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://project.herokuapp.com/api'
+    origins 'http://localhost:3001', 'https://project.herokuapp.com/'
 
     resource '*',
       headers: :any,
